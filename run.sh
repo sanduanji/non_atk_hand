76108cac8cd62a437b8456011f93cc1b2d4ff1cf
calibur <zxd_excalibur@foxmail.com>
2019-04-25 08:29:39 +0800

    change to real non attack

--- run.sh
+++ run.sh
@@ -1,17 +1,5 @@
-INPUT_DIR=
-OUTPUT_DIR=
 MAX_EPSILON=16
 GPU=0
 USE_EXISTING=0
 
-python tar_attack.py\
-	--input_dir = $1 \
-	--output_dir = $2 \
-#	--checkpoint_path = ./checkpoints/ \
-#	--max_epsilon = "${MAX_EPSILON}" \
-#	--num_iter=10 \
-#	--momentum=1.0 \
-#	--use_existing=${USE_EXISTING} \
-#	--gpu=${GPU} \
-#	--batch_size=16 \
-#	--random_eps=1 \
+python3 non_attack.py --input_dir=$1 --output_dir=$2
